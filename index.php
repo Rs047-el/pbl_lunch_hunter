251f7aef1721557ada7c391d267d213c3616d478
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 00:15:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ session_start();
 //require_once 'src/model.php';
 
 $no_header_ouput =[
-    'usr_logout', 'usr_check', 'usr_save'
+    'usr_logout', 'usr_check', 'usr_save', 'rst_save', 'rev_save',
 ];
-?>
+
+$do = $_GET['do'] ?? 'rst_list';
+if(in_array($do, $no_header_ouput)){
+    include "src/{$do}.php";
+} else {
+    include "src/pg_header.php";
+    include "src/{$do}.php";
+    include "src/pg_foter.php";
+}
