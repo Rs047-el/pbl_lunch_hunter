715220898a94cf441e1c30d9d473238b9c4ed23f
k23RS064 <k23rs064@st.kyusan-u.ac.jp>
2025-12-08 09:19:25 +0900

    a

--- src/rev_detail.php
+++ src/rev_detail.php
@@ -12,10 +12,20 @@
         <button>戻る</button>
         <button popovertarget="confirm">通報</button>
         <div id="confirm" popover>
-            <p>本当に通報しますか？</p>
+            <div>通報理由</div>
+            <div>どちらか一つを選択してください。</div>
             <div class="pop-btn-area">
-                <button onclick="location.href='report.php?id=1'">はい</butto>
-                <button popovertarget="confirm" popovertargetaction="hide">いいえ</button>
+                <div>
+                    <label>
+                        <input type="radio">写真
+                    </label>
+                    <label>
+                        <input type="radio">コメント
+                    </label>
+                </div>
+                <div>本当に通報しますか。</div>    
+                <button onclick="location.href='report.php?id=1'">yes</butto>
+                <button popovertarget="confirm" popovertargetaction="hide">no</button>
             </div>
         </div>
     </div>
@@ -25,8 +35,6 @@
     </div>
     <div class="komennto">
         <textarea name="" id="" cols="30" rows="10"></textarea>
-    </div>
-    <div class="phot">
         <img src="" alt="未登録">
         <img src="" alt="未登録">
         <img src="" alt="未登録">
